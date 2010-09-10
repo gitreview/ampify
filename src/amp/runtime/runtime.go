0a439dee27f6a8337f40c0e2cc2f9cef4ec0b199
Yan 'YncYrYdYbYl' Minagawa <t@c-base.org>
2010-09-10 21:11:16 +0200

    just testing the tests on gitreview.com

--- src/amp/runtime/runtime.go
+++ src/amp/runtime/runtime.go
@@ -85,7 +85,7 @@ func GetCPUCount() (count int) {
 		if err != nil {
 			return 1
 		}
-		for _, line := range strings.Split(output, "\n", -1) {
+		for _, line := range strings.Split(output, "\n", 0) {
 			if strings.HasPrefix(line, "processor") {
 				count += 1
 			}
